208fe3a4e9c9e6ba2f77fe8eb8fa884726dd8498
wegizul <mrwg406@gmail.com>
2023-11-01 16:26:24 +0700

    perbaikan jam masuk shift

--- application/controllers/Dashboard.php
+++ application/controllers/Dashboard.php
@@ -31,14 +31,18 @@ class Dashboard extends CI_Controller
 
 		if ($this->session->userdata('level') > 1) {
 			if ($cek) {
-				if (date('D') == "Sat" && $kode_company->cpy_jenis < 3) {
-					$jam_pulang = "12:00:00";
-				} else if ($kode_company->cpy_jenis == 3 && $cek->abs_jam_masuk < "12:00:00") {
-					$jam_pulang = "13:00:00";
-				} else if ($kode_company->cpy_jenis == 3 && $cek->abs_jam_masuk > "13:00:00") {
-					$jam_pulang = "22:00:00";
+				if ($kode_company->cpy_jenis < 2) {
+					if (date('D') == "Sat") {
+						$jam_pulang = "12:00:00";
+					} else {
+						$jam_pulang = "15:00:00";
+					}
 				} else {
-					$jam_pulang = "15:00:00";
+					if ($cek->abs_jam_masuk < "12:00:00") {
+						$jam_pulang = "13:00:00";
+					} else {
+						$jam_pulang = "22:00:00";
+					}
 				}
 			}
 		}
@@ -97,18 +101,20 @@ class Dashboard extends CI_Controller
 		$shift = $this->session->userdata('shift');
 
 		if ($shift == 1) {
-			if (($kode_shift == 1) && (strtotime($waktu_in) < "06:01:00")) {
-				$status = 1;
-			} else if (($kode_shift == 1) && (strtotime($waktu_in) > "06:01:00")) {
-				$terlambat = strtotime($waktu_in) - strtotime("06:01:00");
-				$status = 2;
-			} else if (($kode_shift == 2) && (strtotime($waktu_in) < "13:01:00") && (strtotime($waktu_in) > "08:01:00")) {
-				$status = 1;
-			} else if (($kode_shift == 2) && (strtotime($waktu_in) > "13:01:00")) {
-				$terlambat = strtotime($waktu_in) - strtotime("13:01:00");
-				$status = 2;
+			if ($kode_shift == 1) {
+				if (strtotime($waktu_in) < "06:01:00") {
+					$status = 1;
+				} else {
+					$terlambat = strtotime($waktu_in) - strtotime("06:01:00");
+					$status = 2;
+				}
 			} else {
-				$status = 1; 
+				if (strtotime($waktu_in) > "08:01:00" && strtotime($waktu_in) < "13:01:00") {
+					$status = 1;
+				} else {
+					$terlambat = strtotime($waktu_in) - strtotime("13:01:00");
+					$status = 2;
+				}
 			}
 		} else {
 			if (strtotime($waktu_in) < strtotime($batas_masuk)) {
